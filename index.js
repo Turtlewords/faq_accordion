aba7bbf3af4113f692fc03cf4614f4c8bf3980b3
Turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-05-22 17:54:37 -0500

    start working on js

---
+++ index.js
@@ -0,0 +1,24 @@
+const open = document.querySelectorAll(".open-tab");
+const close = document.querySelectorAll(".close-tab");
+const accordions = document.querySelectorAll(".accordion");
+
+
+for (let i = 0; i < open.length; i++) {
+    open[i].addEventListener("click", function() {
+
+        let panel = open[i].parentElement.nextElementSibling;
+        panel.style.display = "block";
+        open[i].style.display = "none";
+        close[i].style.display = "block";
+    })
+}
+
+for (let i = 0; i < open.length; i++) {
+    close[i].addEventListener("click", function() {
+
+        let panel = open[i].parentElement.nextElementSibling;
+        panel.style.display = "none";
+        close[i].style.display = "none";
+        open[i].style.display = "block";
+    })
+}
