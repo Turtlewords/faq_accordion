0e17f219d64f0f724e6839512e98ccda1787f758
Turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-05-22 18:10:14 -0500

    animate accordion

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ for (let i = 0; i < open.length; i++) {
     open[i].addEventListener("click", function() {
 
         let panel = open[i].parentElement.nextElementSibling;
-        panel.style.display = "block";
+        panel.style.maxHeight = panel.scrollHeight + "px";
         open[i].style.display = "none";
         close[i].style.display = "block";
     })
@@ -17,7 +17,7 @@ for (let i = 0; i < open.length; i++) {
     close[i].addEventListener("click", function() {
 
         let panel = open[i].parentElement.nextElementSibling;
-        panel.style.display = "none";
+        panel.style.maxHeight = null;
         close[i].style.display = "none";
         open[i].style.display = "block";
     })
