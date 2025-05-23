4fad6c42f2fe89f332d38dead4d25c7a3a3949b8
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-05-23 12:31:17 -0500

    first draft

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const open = document.querySelectorAll(".open-tab");
 const close = document.querySelectorAll(".close-tab");
-const accordions = document.querySelectorAll(".accordion");
+const tabs = document.querySelectorAll(".tab");
 
 
-for (let i = 0; i < open.length; i++) {
-    open[i].addEventListener("click", function() {
+for (let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener("click", function() {
 
-        let panel = open[i].parentElement.nextElementSibling;
-        panel.style.maxHeight = panel.scrollHeight + "px";
-        open[i].style.display = "none";
-        close[i].style.display = "block";
+        this.classList.toggle("active");
+        let panel = tabs[i].nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+            close[i].style.display = "none";
+            open[i].style.display = "block";
+            
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+            
+            open[i].style.display = "none";
+            close[i].style.display = "block";
+        }
+        
     })
 }
 
-for (let i = 0; i < open.length; i++) {
-    close[i].addEventListener("click", function() {
-
-        let panel = open[i].parentElement.nextElementSibling;
-        panel.style.maxHeight = null;
-        close[i].style.display = "none";
-        open[i].style.display = "block";
-    })
-}
